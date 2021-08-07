c594ad693e9b0403d4a640bc73b5f711439dcfab
stan000 <stanmpakatig@gmail.com>
2021-08-07 19:07:33 +0200

    everything is tamam

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:ussd_advanced/ussd_advanced.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,39 +13,50 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Choose action'),
-        ),
-        body: Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () => Navigator.pushNamed(context, '/scan'),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                    child: Text(
-                      'Scan QR',
-                      style: TextStyle(fontWeight: FontWeight.w600),
-                    ),
+      appBar: AppBar(
+        title: Text('Choose action'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context, '/scan'),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                  child: Text(
+                    'Scan QR',
+                    style: TextStyle(fontWeight: FontWeight.w600),
                   ),
                 ),
-                SizedBox(height: 40),
-                ElevatedButton(
-                  onPressed: () => Navigator.pushNamed(context, '/generate'),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                    child: Text(
-                      'Generate QR',
-                      style: TextStyle(fontWeight: FontWeight.w600),
-                    ),
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context, '/generate'),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                  child: Text(
+                    'Generate QR',
+                    style: TextStyle(fontWeight: FontWeight.w600),
                   ),
                 ),
-              ],
-            ),
+              ),
+              ElevatedButton(
+                onPressed: () => launch("tel://*151#"),
+                child: Text('Dial something'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  await UssdAdvanced.sendUssd(code: '*151#', subscriptionId: 2);
+                },
+                child: Text('Dial something 2'),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
